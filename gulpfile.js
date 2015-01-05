63b8852f9c02e1cf204a1a1db52f5819b8a7adb5
Jonathan Buchanan <jonathan.buchanan@gmail.com>
2015-01-05 15:31:37 +0000

    Updated build to use insin/templates/react-module

--- gulpfile.js
+++ gulpfile.js
@@ -13,57 +13,54 @@ var uglify = require('gulp-uglify')
 var gutil = require('gulp-util')
 
 var pkg = require('./package.json')
-var dev = gutil.env.production ? '' : ' (dev build at ' + (new Date()).toUTCString() + ')'
-var distHeader = '/**\n\
- * newforms-bootstrap <%= pkg.version %><%= dev %> - https://github.com/insin/newforms-bootstrap\n\
- * MIT Licensed\n\
+var devBuild = gutil.env.production ? '' : ' (dev build at ' + (new Date()).toUTCString() + ')'
+var distHeader = '/*!\n\
+ * <%= pkg.name %> <%= pkg.version %><%= devBuild %> - <%= pkg.homepage %>\n\
+ * <%= pkg.license %> Licensed\n\
  */\n'
 
-var jsSrcPath = './src/**/*.js*'
-var jsLibPath = './lib/**/*.js'
-var jsEntryPoint = './lib/index.js'
+var jsSrcPaths = './src/**/*.js*'
+var jsLibPaths = './lib/**/*.js'
 
 gulp.task('clean-dist', function(cb) {
   del('./dist/*.js', cb)
 })
 
 gulp.task('clean-lib', function(cb) {
-  del('./lib/**/*.js', cb)
+  del(jsLibPaths, cb)
 })
 
 gulp.task('transpile-js', ['clean-lib'], function() {
-  return gulp.src(jsSrcPath)
+  return gulp.src(jsSrcPaths)
     .pipe(plumber())
-    .pipe(react({
-      harmony: true
-    }))
+    .pipe(react({harmony: true}))
     .pipe(gulp.dest('./lib'))
 })
 
 gulp.task('lint-js', ['transpile-js'], function() {
-  return gulp.src(jsLibPath)
+  return gulp.src(jsLibPaths)
     .pipe(jshint('./.jshintrc'))
     .pipe(jshint.reporter('jshint-stylish'))
 })
 
 gulp.task('bundle-js', ['clean-dist', 'lint-js'], function() {
-  var b = browserify(jsEntryPoint, {
-    debug: !gutil.env.production
-  , standalone: 'BootstrapForm'
+  var b = browserify(pkg.main, {
+    debug: !!gutil.env.debug
+  , standalone: pkg.standalone
   , detectGlobals: false
   })
   b.transform('browserify-shim')
 
   var stream = b.bundle()
-    .pipe(source('newforms-bootstrap.js'))
-    .pipe(streamify(header(distHeader, {pkg: pkg, dev: dev})))
+    .pipe(source(pkg.name + '.js'))
+    .pipe(streamify(header(distHeader, {pkg: pkg, devBuild: devBuild})))
     .pipe(gulp.dest('./dist'))
 
   if (gutil.env.production) {
     stream = stream
-      .pipe(rename('newforms-bootstrap.min.js'))
+      .pipe(rename(pkg.name + '.min.js'))
       .pipe(streamify(uglify()))
-      .pipe(streamify(header(distHeader, {pkg: pkg, dev: dev})))
+      .pipe(streamify(header(distHeader, {pkg: pkg, devBuild: devBuild})))
       .pipe(gulp.dest('./dist'))
   }
 
@@ -71,7 +68,7 @@ gulp.task('bundle-js', ['clean-dist', 'lint-js'], function() {
 })
 
 gulp.task('watch', function() {
-  gulp.watch(jsSrcPath, ['bundle-js'])
+  gulp.watch(jsSrcPaths, ['bundle-js'])
 })
 
 gulp.task('default', ['bundle-js', 'watch'])
