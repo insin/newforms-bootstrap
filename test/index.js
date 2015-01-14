7ab3114b392f4b42f32da96b4dc6add6dac6e9da
Jonathan Buchanan <jonathan.buchanan@gmail.com>
2015-01-14 12:01:11 +0000

    Added a test for basic BootstrapForm rendering

--- test/index.js
+++ test/index.js
@@ -1,6 +1,63 @@
+process.env.NODE_ENV = 'production'
+
+var forms = require('newforms')
+var React = require('react')
 var test = require('tape')
 
 var BootstrapForm = require('../lib')
+
+test('BootstrapForm', function(t) {
+  t.plan(1)
+
+  var TestForm = forms.Form.extend({
+    username: forms.CharField(),
+    email: forms.EmailField(),
+    password: forms.CharField({widget: forms.PasswordInput}),
+    confirm: forms.CharField({widget: forms.PasswordInput}),
+
+    clean: function() {
+      var password = this.cleanedData.password
+      var confirm = this.cleanedData.confirm
+      if (password && confirm && password != 'confirm') {
+        throw forms.ValidationError('Passwords do not match.')
+      }
+    }
+  })
+
+  var form = new TestForm({data: {
+    username: 'valid'
+  , email: 'invalid'
+  , password: 'hunter1'
+  , confirm: 'hunter2'
+  }})
+
+  t.equal(
+    React.renderToStaticMarkup(React.createElement(BootstrapForm, {form: form}))
+  , '<div>' +
+      '<div class="alert alert-danger has-error">' +
+        '<span class="help-block"><span class="glyphicon glyphicon-exclamation-sign"></span> Passwords do not match.</span>' +
+      '</div>' +
+      '<div class="form-group has-success">' +
+        '<label class="control-label" for="id_username">Username:</label>' +
+        '<input type="text" name="username" class="form-control" id="id_username" value="valid">' +
+      '</div>' +
+      '<div class="form-group has-error">' +
+        '<label class="control-label" for="id_email">Email:</label>' +
+        '<input type="email" name="email" class="form-control" id="id_email" value="invalid">' +
+        '<span class="help-block"><span class="glyphicon glyphicon-exclamation-sign"></span> Enter a valid email address.</span>' +
+      '</div>' +
+      '<div class="form-group has-success">' +
+        '<label class="control-label" for="id_password">Password:</label>' +
+        '<input type="password" name="password" class="form-control" id="id_password">' +
+      '</div>' +
+      '<div class="form-group has-success">' +
+        '<label class="control-label" for="id_confirm">Confirm:</label>' +
+        '<input type="password" name="confirm" class="form-control" id="id_confirm">' +
+      '</div>' +
+    '</div>'
+  , 'Basic rendering'
+  )
+})
 
 test('PropTypes.colSize', function(t) {
   t.plan(10)
